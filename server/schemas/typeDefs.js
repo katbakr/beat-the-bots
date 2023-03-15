46b6f1aa9c1495995e2e7f746085d7f67d267bbb
Matthew Williams <maw01091989@gmail.com>
2023-03-14 17:45:00 -0700

    added most definitions for the typedefs. no queries or mutations yet

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,8 +1,51 @@
-//Placeholder for commit
-//DELETE UPON BUILD
-
 const typeDefs = `#graphql
+    type User {
+    _id: ID
+    username: String
+    comments: [Comment]
+    }
 
+    type Game {
+    _id: ID
+    joinCode: String
+    users: [User]
+    bots: [Bot]
+    }
+    type Bot {
+    _id: ID
+    botName: String
+    isLocked: Boolean
+    isDefeated: Boolean
+    rounds: [Round]
+    comments: [Comment]
+    }
+
+    input Round {
+    botChoice: String
+    humanChoice: String
+    humanWin: Boolean
+    createdAt: ????
+    }
+
+    type Comment {
+    _id: ID
+    commentText: String
+    commentAuthor: String
+    createdAt: ????
+    }
+
+    type Auth {
+    token: ID!
+    user: User
+    }
+
+    type Query {
+    
+    }
+
+    type Mutation {
+
+    }
 `;
 
 module.exports = typeDefs;
