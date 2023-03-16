d6ffc370fda6cf22cbacabcb58a02e109b4ca148
Katrina Baker <katrinamourine@gmail.com>
2023-03-15 19:50:41 -0700

    starting battle page

---
+++ client/src/pages/Bye.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { useMutation } from "@apollo/client";
+import { Link } from "react-router-dom";
+
+
+function Bye () {
+  
+  return (
+    <div>
+      <Jumbotron>
+        <h1>Hey!</h1>
+        <h2>Thank you for your playing!</h2>
+      </Jumbotron>
+    </div>
+  );
+}
+
+export default Bye;
