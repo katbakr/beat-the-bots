ea40ab99635ba3557f7739e723bdc82ed9128874
Jacob Meyers <jacobmeyers6@outlook.com>
2023-03-18 16:09:07 -0700

    hardcoded roomjoinform into dashboard page to minimize prop handling

--- client/src/components/pages/Dashboard.js
+++ client/src/components/pages/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import RoomJoinForm from "../chat/RoomJoinForm";
 
 export default function Dashboard({ username, room, setRoom, socket }) {
@@ -7,13 +7,41 @@ export default function Dashboard({ username, room, setRoom, socket }) {
    username = 'junk username';
    [room, setRoom] = useState('');
 
+   const navigate = useNavigate();
+
+   // joins the user's socket client to the serverside room
+   const joinRoom = async () => {
+      // room form input must have a value
+      if (room !== '') {
+         // emits event to socket server w/ user data
+         socket.emit('join_room', { username, room });
+
+         // i think redirecting to the battle page from here could work, comment out if not
+         navigate('/battle', { replace: true });
+      }
+   }
+
    return (
       <div className="base">
          <div className='gameBox'>
             <div className='displayBox'>
                <h1 className='homeTitle'> Hi {username}</h1>
 
-               <RoomJoinForm username={username} room={room} setRoom={setRoom} socket={socket} />
+               <div className='formContainer'>
+                  <select
+                     className='roomSelect'
+                     onChange={(e) => setRoom(e.target.value)}
+                  >
+                     <option>--select a room--</option>
+                     {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => (<option value={`Room ${i}`} key={i}>{`Room ${i}`}</option>))}
+                  </select>
+                  <button
+                     className='btn'
+                     onClick={joinRoom}
+                  >
+                     Join Room
+                  </button>
+               </div>
             </div>
          </div>
       </div>
