cff86043a2d62fd960bfc9cfff51117afcfe166e
Katrina Baker <katrinamourine@gmail.com>
2023-03-18 12:09:10 -0700

    fixing battle comments

--- client/src/components/pages/Battle.js
+++ client/src/components/pages/Battle.js
@@ -4,20 +4,8 @@ import Chat from '../chat/Chat.js'
 // import MessageForm from '../chat/MessageForm.js'
 
 export default function Battle({ socket }) {
-    // const choicesList = [
-    //     {
-    //         id: "ROCK",
-    //         image: "../assets/paper.png",
-    //     },
-    //     {
-    //         id: "SCISSORS",
-    //         image: "",
-    //     },
-    //     {
-    //         id: "PAPER",
-    //         image: "",
-    //     },
-    // ];
+    const [botChoice, setBotChocie] = useState("Rock");
+    const [userChoice, setUserChoice] = useState("");
 
     return (
         <div className="base">
@@ -36,7 +24,8 @@ export default function Battle({ socket }) {
                                 {/* choicesList={choicesList} */}
                             </div>
                             <div className="userChoices">
-                                <div className="choice">
+
+                                 <div className="choice">
                                     <img src='./assets/rock.png'></img>
                                     <div className="btn1">
                                     <button className="rockBtn"></button>
@@ -62,80 +51,83 @@ export default function Battle({ socket }) {
                     </div>
                 </div>
             </div>
+            </div>
+            )
+            };
 //==================================================================================================================================================
-// import { Link } from "react-router-dom";
+{/* // import { Link } from "react-router-dom";
 
-import "./battle.css";
+// import "./battle.css";
 
-function Battle() {
-  // code for taking user data and the pass to Bot utils?
-  const [botChoice, setBotChocie] = useState("Rock");
-  const [userChoice, setUserChoice] = useState("");
+// function Battle() { */}
+{/* // code for taking user data and the pass to Bot utils? */}
+   {/* const [botChoice, setBotChocie] = useState("Rock");
+   const [userChoice, setUserChoice] = useState("");
 
-  return (
-    <div>
-      <div className="header">
-        <h1>Let's Play!</h1>
-      </div>
-      <div className="botCard">
-        <h3>Taunt for Bot</h3>
+   return (
+     <div>
+       <div className="header">
+         <h1>Let's Play!</h1>
+       </div>
+       <div className="botCard">
+         <h3>Taunt for Bot</h3>
 
-        <div>
-          <button>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span> ROCK
-          </button>
-          <button>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span> PAPER
-          </button>
-          <button>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span> SCISSORS
-          </button>
-          <button type="button" class="btn">
-            <strong>ROCK</strong>
-            <div id="container-stars">
-              <div id="stars"></div>
-            </div>
+         <div>
+           <button>
+             <span></span>
+             <span></span>
+             <span></span>
+             <span></span> ROCK
+           </button>
+           <button>
+             <span></span>
+             <span></span>
+             <span></span>
+             <span></span> PAPER
+           </button>
+           <button>
+             <span></span>
+             <span></span>
+             <span></span>
+             <span></span> SCISSORS
+           </button>
+           <button type="button" class="btn">
+             <strong>ROCK</strong>
+             <div id="container-stars">
+               <div id="stars"></div>
+             </div>
 
-            <div id="glow">
-              <div class="circle"></div>
-              <div class="circle"></div>
-            </div>
-          </button>
-          <button type="button" class="btn">
-            <strong>PAPER</strong>
-            <div id="container-stars">
-              <div id="stars"></div>
-            </div>
+             <div id="glow">
+               <div class="circle"></div>
+               <div class="circle"></div>
+             </div>
+           </button>
+           <button type="button" class="btn">
+             <strong>PAPER</strong>
+             <div id="container-stars">
+               <div id="stars"></div>
+             </div>
 
-            <div id="glow">
-              <div class="circle"></div>
-              <div class="circle"></div>
-            </div>
-          </button>
-          <button type="button" class="btn">
-            <strong>SCISSORS</strong>
-            <div id="container-stars">
-              <div id="stars"></div>
-            </div>
+             <div id="glow">
+               <div class="circle"></div>
+               <div class="circle"></div>
+             </div>
+           </button>
+           <button type="button" class="btn">
+             <strong>SCISSORS</strong>
+             <div id="container-stars">
+               <div id="stars"></div>
+             </div>
 
-            <div id="glow">
-              <div class="circle"></div>
-              <div class="circle"></div>
-            </div>
-          </button>
-        </div>
-      </div>
-      <div className="commentBox">Comments</div>
-    </div>
-  );
-}
-
+             <div id="glow">
+               <div class="circle"></div>
+               <div class="circle"></div>
+             </div>
+           </button>
+         </div>
+       </div>
+       <div className="commentBox">Comments</div>
+     </div>
+   );
+ }
+ export default Battle; */}
