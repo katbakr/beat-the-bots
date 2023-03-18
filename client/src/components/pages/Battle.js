53171c00c584078f90c91a4e7acf33106ee96f65
Katrina Baker <katrinamourine@gmail.com>
2023-03-18 12:46:10 -0700

    temporary battle comments

--- client/src/components/pages/Battle.js
+++ client/src/components/pages/Battle.js
@@ -6,83 +6,84 @@ import Chat from '../chat/Chat.js'
 export default function Battle({ socket }) {
     const [botChoice, setBotChocie] = useState("Rock");
     const [userChoice, setUserChoice] = useState("");
-function Battle() {
-  // code for taking user data and the pass to Bot utils?
-  //need a query and mutation for passing results to user in database, and getting win streak
-  const [botChoice, setBotChocie] = useState("Rock");
-  const [userChoice, setUserChoice] = useState("");
-  const [winner, setWinner] = useState("");
 
-  const checkWinner = () => {};
+// function Battle() {
+//   // code for taking user data and the pass to Bot utils?
+//   //need a query and mutation for passing results to user in database, and getting win streak
+//   const [botChoice, setBotChocie] = useState("Rock");
+//   const [userChoice, setUserChoice] = useState("");
+//   const [winner, setWinner] = useState("");
 
-  return (
-    <div>
-      <div className="header">
-        <h1>Let's Play!</h1>
-      </div>
-      <div className="botCard">
-        <h3>Taunt for Bot</h3>
+//   const checkWinner = () => {};
 
-        <div>
-          <button>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span> ROCK
-          </button>
-          <button>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span> PAPER
-          </button>
-          <button>
-            <span></span>
-            <span></span>
-            <span></span>
-            <span></span> SCISSORS
-          </button>
-          <div style={{ display: "flex" }}>
-            <button type="button" class="btn">
-              <strong>ROCK</strong>
-              <div id="container-stars">
-                <div id="stars"></div>
-              </div>
+//   return (
+//     <div>
+//       <div className="header">
+//         <h1>Let's Play!</h1>
+//       </div>
+//       <div className="botCard">
+//         <h3>Taunt for Bot</h3>
 
-              <div id="glow">
-                <div class="circle"></div>
-                <div class="circle"></div>
-              </div>
-            </button>
-            <button type="button" class="btn">
-              <strong>PAPER</strong>
-              <div id="container-stars">
-                <div id="stars"></div>
-              </div>
+//         <div>
+//           <button>
+//             <span></span>
+//             <span></span>
+//             <span></span>
+//             <span></span> ROCK
+//           </button>
+//           <button>
+//             <span></span>
+//             <span></span>
+//             <span></span>
+//             <span></span> PAPER
+//           </button>
+//           <button>
+//             <span></span>
+//             <span></span>
+//             <span></span>
+//             <span></span> SCISSORS
+//           </button>
+//           <div style={{ display: "flex" }}>
+//             <button type="button" class="btn">
+//               <strong>ROCK</strong>
+//               <div id="container-stars">
+//                 <div id="stars"></div>
+//               </div>
 
-              <div id="glow">
-                <div class="circle"></div>
-                <div class="circle"></div>
-              </div>
-            </button>
-            <button type="button" class="btn">
-              <strong>SCISSORS</strong>
-              <div id="container-stars">
-                <div id="stars"></div>
-              </div>
+//               <div id="glow">
+//                 <div class="circle"></div>
+//                 <div class="circle"></div>
+//               </div>
+//             </button>
+//             <button type="button" class="btn">
+//               <strong>PAPER</strong>
+//               <div id="container-stars">
+//                 <div id="stars"></div>
+//               </div>
 
-              <div id="glow">
-                <div class="circle"></div>
-                <div class="circle"></div>
-              </div>
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="commentBox">Comments</div>
-    </div>
-  );
-}
+//               <div id="glow">
+//                 <div class="circle"></div>
+//                 <div class="circle"></div>
+//               </div>
+//             </button>
+//             <button type="button" class="btn">
+//               <strong>SCISSORS</strong>
+//               <div id="container-stars">
+//                 <div id="stars"></div>
+//               </div>
+
+//               <div id="glow">
+//                 <div class="circle"></div>
+//                 <div class="circle"></div>
+//               </div>
+//             </button>
+//           </div>
+//         </div>
+//       </div>
+//       <div className="commentBox">Comments</div>
+//     </div>
+//   );
+// }
 //=======================================================================================================================
     return (
         <div className="base">
