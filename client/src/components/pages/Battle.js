1f5736d225dc2980a13b05bdf750e9c0a93f71cb
Matthew Williams <maw01091989@gmail.com>
2023-03-19 08:38:22 -0700

    added button click functionality on battle page

--- client/src/components/pages/Battle.js
+++ client/src/components/pages/Battle.js
@@ -21,62 +21,75 @@ export default function Battle({ socket }) {
     }
   };
 
-  const handleClick = async (e) => {
-    e.preventDefault();
-
-    setUserChoice(e.target.value);
+  const handleClick = async (choice) => {
+    setUserChoice(choice);
 
     checkWinner(botChoice, userChoice);
   };
 
-// Kat Code =======================================================================================================================
-    return (
-        <div className="base">
-            <div className="gameBox" id='display'>
-                <div>
-                    <div className="titleContainer">
-                        <h1 className="battleTitle">Time to Battle!</h1>
-                    </div>
-                    <div className='playContainer'>
-                        <div className="botBox">
-                            <div className='botImg'>
-                                <img src='./assets/bot1.png'></img>
-                            </div>
-                            <div className="botCard">
-                                <h3 className="botTaunt">I love rocks. Aren't rocks the coolest?</h3>
-                                {/* choicesList={choicesList} */}
-                            </div>
-                            <div className="userChoices">
-
-                                 <div className="choice">
-                                    <img src='./assets/rock.png'></img>
-                                    <div className="btn1">
-                                    <button className="rockBtn"></button>
-                                    </div>
-                                </div>
-                                <div className="choice" id='btn2'>
-                                    <img src='./assets/paper.png'></img>
-                                    <div className="btn2">
-                                    <button className="paperBtn"></button>
-                                    </div>
-                                </div>
-                                <div className="choice">
-                                    <img src='./assets/scissors.png'></img>
-                                    <div className="btn3">
-                                    <button className="scissorsBtn"></button>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="chatBox">
-                            < Chat socket={socket} username='horseTeeth' room='room 1' />
-                        </div>
-                    </div>
+  // Kat Code =======================================================================================================================
+  return (
+    <div className="base">
+      <div className="gameBox" id="display">
+        <div>
+          <div className="titleContainer">
+            <h1 className="battleTitle">Time to Battle!</h1>
+          </div>
+          <div className="playContainer">
+            <div className="botBox">
+              <div className="botImg">
+                <img src="./assets/bot1.png"></img>
+              </div>
+              <div className="botCard">
+                <h3 className="botTaunt">
+                  I love rocks. Aren't rocks the coolest?
+                </h3>
+                {/* choicesList={choicesList} */}
+              </div>
+              <div className="userChoices">
+                <div className="choice">
+                  <img src="./assets/rock.png"></img>
+                  <div className="btn1">
+                    <button
+                      className="rockBtn"
+                      onClick={() => handleClick("Rock")}
+                    ></button>
+                  </div>
                 </div>
+                <div className="choice" id="btn2">
+                  <img src="./assets/paper.png"></img>
+                  <div className="btn2">
+                    <button
+                      className="paperBtn"
+                      onClick={() => handleClick("PAPER")}
+                    ></button>
+                  </div>
+                </div>
+                <div className="choice">
+                  <img src="./assets/scissors.png"></img>
+                  <div className="btn3">
+                    <button
+                      className="scissorsBtn"
+                      onClick={() => handleClick("SCISSORS")}
+                    ></button>
+                  </div>
+                </div>
+              </div>
             </div>
+            <div className="chatBox">
+              <Chat socket={socket} username="horseTeeth" room="room 1" />
             </div>
-            )
-            };
+            <div style={{ display: "flex", color: "white" }}>
+              <p>{userChoice}</p>
+              <p>{botChoice}</p>
+              <p>{winner}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 //Matt Code ==================================================================================================================================================
 //   // code for taking user data and the pass to Bot utils?
 //   //need a query and mutation for passing results to user in database, and getting win streak
