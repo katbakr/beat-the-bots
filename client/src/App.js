a6518872c863b69268fa78257253a9f34c7138ff
Rhys Smoker <r.smoker@gmail.com>
2023-03-19 11:31:14 -0700

    fixed dashboard route in app.js so dashboard now says hi junk username select a room join room

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  createHttpLink,
+	ApolloClient,
+	InMemoryCache,
+	ApolloProvider,
+	createHttpLink,
 } from "@apollo/client";
 import { setContext } from "@apollo/client/link/context";
 import "./App.css";
@@ -21,45 +21,63 @@ import io from "socket.io-client";
 const socket = io();
 
 const httpLink = createHttpLink({
-  uri: "/graphql",
+	uri: "/graphql",
 });
 
 const authLink = setContext((_, { headers }) => {
-  const token = localStorage.getItem("id_token");
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : "",
-    },
-  };
+	const token = localStorage.getItem("id_token");
+	return {
+		headers: {
+			...headers,
+			authorization: token ? `Bearer ${token}` : "",
+		},
+	};
 });
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
+	link: authLink.concat(httpLink),
+	cache: new InMemoryCache(),
 });
 
 function App() {
-  return (
-
-    <div  className="base">
-      <div className='gameBox'>
-        <div className='displayBox'>
-          <ApolloProvider client={client}>
-            <Router>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Signup />} />
-                <Route path="/battle" element={<Battle socket={socket} />} />
-                <Route path="*" element={<Bye />} />
-              </Routes>
-            </Router>
-          </ApolloProvider>
-        </div>
-      </div>
-  </div>
-  );
+	return (
+		<div className="base">
+			<div className="gameBox">
+				<div className="displayBox">
+					<ApolloProvider client={client}>
+						<Router>
+							<Routes>
+								<Route
+									path="/"
+									element={<Home />}
+								/>
+								<Route
+									path="/login"
+									element={<Login />}
+								/>
+								<Route
+									path="/signup"
+									element={<Signup />}
+								/>
+								<Route
+									path="/battle"
+									element={<Battle socket={socket} />}
+								/>
+								<Route
+									path="/dashboard"
+									element={<Dashboard />}
+								/>
+								<Route
+									path="*"
+									element={<Bye />}
+								/>
+							</Routes>
+						</Router>
+					</ApolloProvider>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default App;
