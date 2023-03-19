d3f6f179f566e0c2085d8ffa4c5e5981e95a1fe0
Rhys Smoker <r.smoker@gmail.com>
2023-03-19 10:06:35 -0700

    gave the text in bye.js gameBox styling so the text can be seen against the dark background

--- client/src/components/pages/Bye.js
+++ client/src/components/pages/Bye.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 import { useMutation } from "@apollo/client";
 import { Link } from "react-router-dom";
 
-
-function Bye () {
-  
-  return (
-    <div>
-        <h1>Hey!</h1>
-        <h2>Thank you for your playing!</h2>
-    </div>
-  );
+function Bye() {
+	return (
+		<div className="gameBox">
+			{" "}
+			<div>
+				<h1 className="homeTitle">Hey!</h1>
+				<h3>Thank you for your playing!</h3>
+			</div>
+		</div>
+	);
 }
 
 export default Bye;
