50dd13aa9441f83d0400510ef3665238a2521c65
Matthew Williams <maw01091989@gmail.com>
2023-03-20 15:22:09 -0700

    removed default state for botChoice, and added a random selected for first turn

--- client/src/components/pages/Battle.js
+++ client/src/components/pages/Battle.js
@@ -2,100 +2,107 @@ import { Component, useState, useEffect } from "react";
 import { useMutation } from "@apollo/client";
 import Chat from "../chat/Chat.js";
 
-import './battle.css';
+import "./battle.css";
 import io from "socket.io-client";
 
 const socket = io();
 
 export default function Battle({ username }) {
-	const [botChoice, setBotChoice] = useState("ROCK");
-	const [userChoice, setUserChoice] = useState("");
-	const [winnerState, setWinnerState] = useState("No Winner Yet");
+  const [botChoice, setBotChoice] = useState("");
+  const [userChoice, setUserChoice] = useState("");
+  const [winnerState, setWinnerState] = useState("No Winner Yet");
 
-	const checkWinner = () => {
-		if (
-			(userChoice === "PAPER" && botChoice === "ROCK") ||
-			(userChoice === "ROCK" && botChoice === "SCISSORS") ||
-			(userChoice === "SCISSORS" && botChoice === "PAPER")
-		) {
-			setWinnerState("Player Wins");
-		} else {
-			setWinnerState("Robot Wins");
-		}
-	};
+  const checkWinner = () => {
+    if (
+      (userChoice === "PAPER" && botChoice === "ROCK") ||
+      (userChoice === "ROCK" && botChoice === "SCISSORS") ||
+      (userChoice === "SCISSORS" && botChoice === "PAPER")
+    ) {
+      setWinnerState("Player Wins");
+    } else {
+      setWinnerState("Robot Wins");
+    }
+  };
 
-	useEffect(() => {
-		socket.emit('join_room', { username, room: 'Public' });
-	}, [])
+  useEffect(() => {
+    socket.emit("join_room", { username, room: "Public" });
+  }, []);
 
-	//this says "re-run the checkWinner() function every time either userChoice or botChoice changes"
-	useEffect(() => {
-		checkWinner();
-	}, [userChoice, botChoice]);
+  //this says "re-run the checkWinner() function every time either userChoice or botChoice changes"
+  useEffect(() => {
+    checkWinner();
+  }, [userChoice, botChoice]);
 
-	const handleClick = (choice) => {
-   const choices = ["ROCK", "PAPER", "SCISSORS"];
-    const index = choices.indexOf(botChoice);
+  const handleClick = (choice) => {
+    const choices = ["ROCK", "PAPER", "SCISSORS"];
+    function getRandomChoice() {
+      return choices[Math.floor(Math.random() * choices.length)];
+    }
+
+    const index = choices.indexOf(botChoice || getRandomChoice());
     const botChoiceIndex = choices[(index + 1) % choices.length];
     setBotChoice(botChoiceIndex);
-  
-		setUserChoice(choice);
-	};
 
-	return (
-		// <div className="base">
-		<div
-		// className="gameBox"
-		// id="display"
-		>
-			<div>
-				<div className="titleContainer">
-					<h1 className="battleTitle">Time to Battle!</h1>
-				</div>
-				<div className="playContainer">
-					<div className="botBox">
-						<div className="botImg">
-							<img src="./assets/bot1.png"></img>
-						</div>
-						<div className="botCard">
-							<h4 className="botTaunt">
-								I love rocks. Aren't rocks the coolest?
-							</h4>
-						</div>
-						<div className="userChoices">
-							<div className="choice">
-								{/* <img src="./assets/rock.png"></img> */}
-								{/* <div className="btn1"> */}
-								<button className="button" id="rockBtn2"
-									onClick={() => handleClick("ROCK")}>
-									ROCK
-									<span></span>
-									<span></span>
-									<span></span>
-									<span></span>
-								</button>
+    setUserChoice(choice);
+  };
 
-								{/* <button
+  return (
+    // <div className="base">
+    <div
+    // className="gameBox"
+    // id="display"
+    >
+      <div>
+        <div className="titleContainer">
+          <h1 className="battleTitle">Time to Battle!</h1>
+        </div>
+        <div className="playContainer">
+          <div className="botBox">
+            <div className="botImg">
+              <img src="./assets/bot1.png"></img>
+            </div>
+            <div className="botCard">
+              <h4 className="botTaunt">
+                I love rocks. Aren't rocks the coolest?
+              </h4>
+            </div>
+            <div className="userChoices">
+              <div className="choice">
+                {/* <img src="./assets/rock.png"></img> */}
+                {/* <div className="btn1"> */}
+                <button
+                  className="button"
+                  id="rockBtn2"
+                  onClick={() => handleClick("ROCK")}
+                >
+                  ROCK
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </button>
+
+                {/* <button
 											className="rockBtn"
 											onClick={() => handleClick("ROCK")}>
 											Ro
 										</button> */}
-								{/* </div> */}
-							</div>
-							<div
-								className="choice"
-								id="btn2">
+                {/* </div> */}
+              </div>
+              <div className="choice" id="btn2">
+                <button
+                  className="button"
+                  id="paperBtn2"
+                  onClick={() => handleClick("PAPER")}
+                >
+                  PAPER
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </button>
 
-								<button className="button" id="paperBtn2"
-									onClick={() => handleClick("PAPER")}>
-									PAPER
-									<span></span>
-									<span></span>
-									<span></span>
-									<span></span>
-								</button>
-
-								{/* <img src="./assets/paper.png"></img>
+                {/* <img src="./assets/paper.png"></img>
 									<div className="btn2">
 										<button
 											className="paperBtn"
@@ -103,19 +110,21 @@ export default function Battle({ username }) {
 											Pa
 										</button>
 									</div> */}
-							</div>
-							<div className="choice">
+              </div>
+              <div className="choice">
+                <button
+                  className="button"
+                  id="scissorsBtn2"
+                  onClick={() => handleClick("SCISSORS")}
+                >
+                  Scissors
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </button>
 
-								<button className="button" id="scissorsBtn2"
-									onClick={() => handleClick("SCISSORS")}>
-									Scissors
-									<span></span>
-									<span></span>
-									<span></span>
-									<span></span>
-								</button>
-
-								{/* <img src="./assets/scissors.png"></img>
+                {/* <img src="./assets/scissors.png"></img>
 									<div className="btn3">
 										<button
 											className="scissorsBtn"
@@ -123,29 +132,23 @@ export default function Battle({ username }) {
 											Sc
 										</button>
 									</div> */}
-
-							</div>
-						</div>
-					</div>
-					<div className="rightColumn">
-						<div className="gameStats">
-							<h1>Round Results</h1>
-							<p>Player chose: {userChoice}</p>
-							<p>Bot chose: {botChoice}</p>
-							<p>The Winner is: {winnerState}</p>
-						</div>
-						<div className="chatBox">
-							<Chat
-								socket={socket}
-								username={username}
-								room="Public"
-							/>
-
-						</div>
-					</div>
-				</div>
-			</div>
-		</div >
-		// </div>
-	);
+              </div>
+            </div>
+          </div>
+          <div className="rightColumn">
+            <div className="gameStats">
+              <h1>Round Results</h1>
+              <p>Player chose: {userChoice}</p>
+              <p>Bot chose: {botChoice}</p>
+              <p>The Winner is: {winnerState}</p>
+            </div>
+            <div className="chatBox">
+              <Chat socket={socket} username={username} room="Public" />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    // </div>
+  );
 }
