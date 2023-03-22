3303ce904b63be82a5232c2aeaf3419798a024d7
Jacob Meyers <jacobmeyers6@outlook.com>
2023-03-21 18:31:46 -0700

    set up env files

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
+require('dotenv').config();
 
-const secret = "o38ygqo8uhqoeirh";
+const secret = process.env.SECRET;
 const expiration = "2h";
 
 module.exports = {
